5437e08ce0ab8dd609ccc3e468f1456518dfb8c6
Lauri Himanen <lauri.himanen@aalto.fi>
2016-12-02 11:51:56 +0200

    Removed additional letter V from test spec file.

--- src/test/scala/eu/nomad_lab/parsers/BigDFTParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/BigDFTParserSpec.scala
@@ -6,7 +6,7 @@ object BigDFTParserSpec extends Specification {
   "BigDFTParserTest" >> {
     "test with json-events" >> {
       ParserRun.parse(BigDFTParser, "parsers/big-dft/test/examples/single_point/output.out", "json-events") must_== ParseResult.ParseSuccess
-    V}
+    }
   }
 
   "test single_point with json" >> {
